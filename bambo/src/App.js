88199450b5c419973e6dbdb22353f8ac626b73f7
kracak <scislokuba@gmail.com>
2023-10-23 11:04:52 +0200

    Dodano FireBase

---
+++ bambo/src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDD83UzInf0ytN0oCt80UVtSwZMOqAZu7s",
+  authDomain: "bambopl-65a7a.firebaseapp.com",
+  projectId: "bambopl-65a7a",
+  storageBucket: "bambopl-65a7a.appspot.com",
+  messagingSenderId: "387024769804",
+  appId: "1:387024769804:web:b52b036eab0637faebef29",
+  measurementId: "G-EKEG1V5SGP"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+function App() {
+  return (
+    <div className="App">
+     
+    </div>
+  );
+}
+
+export default App;
